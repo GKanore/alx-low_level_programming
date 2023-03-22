c9edfedf30349935f0a4dac821686c76bb2a5e1a
kanore <derro91@gmail.com>
2023-03-22 16:43:00 -0700

    First 98 Fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
-
+#define LARGEST 10000000000
 /**
-  * main - gets sum of all even number fibonacci < 4mill
-  * Return: void
-  */
+ * main - main block
+ * Description: Find and print the first 98 fib numbers starting with 1 and 2.
+ * Numbers should be coma and space separated.
+ * Return: 0
+ */
 int main(void)
 {
-	/* @pp: F(n-2), @prev: F(n-1), @fib: F(n) */
-	long int pp = 0, prev = 1, fib = 0, sum = 0;
-	int counter = 0;
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
 
-	while (fib < 4000000)
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
 	{
-		/* get the fib of n, set @pp and @prev */
-		fib = pp + prev;
-		pp = prev;
-		prev = fib;
-
-		/* check if fib is even & add */
-		if (fib % 2 == 0)
-			sum += fib;
-		counter++;
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
+			printf(", ");
 	}
-	printf("%li\n", sum);
+	printf("\n");
 	return (0);
 }
