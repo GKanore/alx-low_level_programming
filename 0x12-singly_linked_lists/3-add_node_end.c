0958d318e330d6d53dae4abcc66102dee1360e55
Kanore <derro91@gmail.com>
2023-04-29 03:00:44 -0700

    Add Node End

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,8 +1,9 @@
 #include "lists.h"
+#include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
+
 /**
- * add_node_end - add a new node at the end of `list_t` list
+ * add_node_end - add a new node at the end of a list_t list
  * @head: double pointer to head
  * @str: string to duplicate into new node
  * Return: Address of the new element or NULL if failed
@@ -17,13 +18,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	while (current && current->next != NULL)
 		current = current->next;
 
-	for (c = 0; dup_str[c] != '\0'; c++)
-		;
+	for (c = 0; str[c] != '\0'; c++);
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	{
-		free(new_node);
 		return (NULL);
 	}
 	new_node->str = strdup(str);
@@ -35,9 +34,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = c;
 	new_node->next = NULL;
 
-	if (current)
-		current->next = new_node;
-	else
+	if (*head == NULL)
+	{
 		*head = new_node;
+	}
+	else
+	{
+		current->next = new_node;
+	}
+
 	return (new_node);
 }
